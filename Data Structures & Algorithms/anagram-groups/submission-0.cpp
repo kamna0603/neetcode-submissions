22cd11d5279019b68a60ff7ae6fdb9ebaee1c030
KAMNA CHAUHAN <kamnac78@gmail.com>
2026-08-28 23:57:10 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<vector<int>, vector<string>> mp;
+        for(string s:strs){
+            vector<int>count(26,0);
+            for( char ch:s){
+                count[ch-'a']++;
+            }
+            mp[count].push_back(s);
+
+        }
+        vector<vector<string>>ans;
+        for(auto x: mp){
+         ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
+
+
